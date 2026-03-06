1b302bd3fc508b50cc5414a768edcf4ff76218fa
TazBrks <tasnimbourkhis@gmail.com>
2026-03-06 13:36:19 +0100

    Remove gps variable declaration from variables.hpp

--- src/variables.hpp
+++ src/variables.hpp
@@ -34,7 +34,6 @@ extern const char* serverName;
 #pragma endregion
 
 #pragma GPS variable
-extern TinyGPSPlus gps;
 extern HardwareSerial gpsSerial;
 extern double latitude;
 extern double longitude;
